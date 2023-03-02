f4998a0c38aed93887139689973613a9f98d5270
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-03-02 18:19:10 +0600

    user cubit state created

---
+++ lib/modules/user/cubit/user_cubit_state.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+
+import '../model/user/user.dart';
+
+abstract class UserCubitState extends Equatable {
+  const UserCubitState();
+}
+
+class UserCubitInit extends UserCubitState {
+  @override
+  List<Object?> get props => [];
+}
+
+class UserCubitLoading extends UserCubitState {
+  @override
+  List<Object?> get props => [];
+}
+
+// ignore: must_be_immutable
+class UserCubitDataLoaded extends UserCubitState {
+  List<User> userList;
+
+  UserCubitDataLoaded({required this.userList});
+
+  @override
+  List<Object?> get props => [userList];
+}
+
+// ignore: must_be_immutable
+class UserCubitError extends UserCubitState {
+  String message;
+  UserCubitError({required this.message});
+  @override
+  List<Object?> get props => [];
+}
