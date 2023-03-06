1cc55210fcecbd514ff9e7c828db8eaae8cd29b4
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-03-06 18:23:54 +0600

    user model updated

--- lib/modules/user/model/user/user.dart
+++ lib/modules/user/model/user/user.dart
@@ -1,111 +1,59 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
-
-import 'address.dart';
-import 'company.dart';
 
 class UserModel {
   int? id;
   String? name;
   String? username;
-  String? email;
-  Address? address;
-  String? phone;
-  String? website;
-  Company? company;
-
   UserModel({
     this.id,
     this.name,
     this.username,
-    this.email,
-    this.address,
-    this.phone,
-    this.website,
-    this.company,
   });
-
-  @override
-  String toString() {
-    return 'User(id: $id, name: $name, username: $username, email: $email, address: $address, phone: $phone, website: $website, company: $company)';
-  }
-
-  factory UserModel.fromId1NameLeanneGrahamUsernameBretEmailSincereAprilBizAddressStreetKulasLightSuiteApt556CityGwenboroughZipcode929983874GeoLat373159Lng811496Phone17707368031X56442WebsiteHildegardOrgCompanyNameRomagueraCronaCatchPhraseMultiLayeredClientServerNeuralNetBsHarnessRealTimeEMarkets(
-      Map<String, dynamic> data) {
-    return UserModel(
-      id: data['id'] as int?,
-      name: data['name'] as String?,
-      username: data['username'] as String?,
-      email: data['email'] as String?,
-      address: data['address'] == null
-          ? null
-          : Address
-              .fromId1NameLeanneGrahamUsernameBretEmailSincereAprilBizAddressStreetKulasLightSuiteApt556CityGwenboroughZipcode929983874GeoLat373159Lng811496Phone17707368031X56442WebsiteHildegardOrgCompanyNameRomagueraCronaCatchPhraseMultiLayeredClientServerNeuralNetBsHarnessRealTimeEMarkets(
-                  data['address'] as Map<String, dynamic>),
-      phone: data['phone'] as String?,
-      website: data['website'] as String?,
-      company: data['company'] == null
-          ? null
-          : Company
-              .fromId1NameLeanneGrahamUsernameBretEmailSincereAprilBizAddressStreetKulasLightSuiteApt556CityGwenboroughZipcode929983874GeoLat373159Lng811496Phone17707368031X56442WebsiteHildegardOrgCompanyNameRomagueraCronaCatchPhraseMultiLayeredClientServerNeuralNetBsHarnessRealTimeEMarkets(
-                  data['company'] as Map<String, dynamic>),
-    );
-  }
-
-  Map<String, dynamic>
-      toId1NameLeanneGrahamUsernameBretEmailSincereAprilBizAddressStreetKulasLightSuiteApt556CityGwenboroughZipcode929983874GeoLat373159Lng811496Phone17707368031X56442WebsiteHildegardOrgCompanyNameRomagueraCronaCatchPhraseMultiLayeredClientServerNeuralNetBsHarnessRealTimeEMarkets() {
-    return {
-      'id': id,
-      'name': name,
-      'username': username,
-      'email': email,
-      'address': address
-          ?.toId1NameLeanneGrahamUsernameBretEmailSincereAprilBizAddressStreetKulasLightSuiteApt556CityGwenboroughZipcode929983874GeoLat373159Lng811496Phone17707368031X56442WebsiteHildegardOrgCompanyNameRomagueraCronaCatchPhraseMultiLayeredClientServerNeuralNetBsHarnessRealTimeEMarkets(),
-      'phone': phone,
-      'website': website,
-      'company': company
-          ?.toId1NameLeanneGrahamUsernameBretEmailSincereAprilBizAddressStreetKulasLightSuiteApt556CityGwenboroughZipcode929983874GeoLat373159Lng811496Phone17707368031X56442WebsiteHildegardOrgCompanyNameRomagueraCronaCatchPhraseMultiLayeredClientServerNeuralNetBsHarnessRealTimeEMarkets(),
-    };
-  }
-
-  factory UserModel.fromJson(String data) {
-    return UserModel
-        .fromId1NameLeanneGrahamUsernameBretEmailSincereAprilBizAddressStreetKulasLightSuiteApt556CityGwenboroughZipcode929983874GeoLat373159Lng811496Phone17707368031X56442WebsiteHildegardOrgCompanyNameRomagueraCronaCatchPhraseMultiLayeredClientServerNeuralNetBsHarnessRealTimeEMarkets(
-            json.decode(data) as Map<String, dynamic>);
-  }
-
-  String toJson() => json.encode(
-      toId1NameLeanneGrahamUsernameBretEmailSincereAprilBizAddressStreetKulasLightSuiteApt556CityGwenboroughZipcode929983874GeoLat373159Lng811496Phone17707368031X56442WebsiteHildegardOrgCompanyNameRomagueraCronaCatchPhraseMultiLayeredClientServerNeuralNetBsHarnessRealTimeEMarkets());
 
   UserModel copyWith({
     int? id,
     String? name,
     String? username,
-    String? email,
-    Address? address,
-    String? phone,
-    String? website,
-    Company? company,
   }) {
     return UserModel(
       id: id ?? this.id,
       name: name ?? this.name,
       username: username ?? this.username,
-      email: email ?? this.email,
-      address: address ?? this.address,
-      phone: phone ?? this.phone,
-      website: website ?? this.website,
-      company: company ?? this.company,
     );
   }
 
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'username': username,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'] != null ? map['id'] as int : null,
+      name: map['name'] != null ? map['name'] as String : null,
+      username: map['username'] != null ? map['username'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
   @override
-  int get hashCode =>
-      id.hashCode ^
-      name.hashCode ^
-      username.hashCode ^
-      email.hashCode ^
-      address.hashCode ^
-      phone.hashCode ^
-      website.hashCode ^
-      company.hashCode;
+  String toString() => 'UserModel(id: $id, name: $name, username: $username)';
+
+  @override
+  bool operator ==(covariant UserModel other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id && other.name == name && other.username == username;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ name.hashCode ^ username.hashCode;
 }
