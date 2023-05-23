6c612a794ba7e54d418c978dca820d8a806ca4e1
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-05-23 12:19:42 +0600

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'package:everything_about_flutter_bloc/modules/user/cubit/user_cubit.dart';
-import 'package:everything_about_flutter_bloc/service/api_service.dart';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'modules/user/view/user_view.dart';
+import 'modules/counter/counter_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,17 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => UserCubit(apiService: ApiService()))
-      ],
-      child: MaterialApp(
-        title: 'FLUTTER BLOC',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const UserView(),
+    return MaterialApp(
+      title: 'FLUTTER BLOC',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const CounterView(),
     );
   }
 }
