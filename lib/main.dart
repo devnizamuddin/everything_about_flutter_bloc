d411c2aaf0ef75ef96b318f59e420850b685ddce
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-05-29 11:00:01 +0600

    counter bloc added over material app for using in counter view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:everything_about_flutter_bloc/modules/counter/bloc/counter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'modules/counter/counter_view.dart';
 
 void main() {
@@ -10,12 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FLUTTER BLOC',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => CounterBloc())],
+      child: MaterialApp(
+        title: 'FLUTTER BLOC',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const CounterView(),
       ),
-      home: const CounterView(),
     );
   }
 }
