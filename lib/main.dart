e71ed4ea8142cc8393c2db3f8aa70ebffde3fe07
md.nizamuddinshamrat <pridesys.0027@gmail.com>
2023-05-29 16:17:26 +0600

    user bloc added over material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:everything_about_flutter_bloc/modules/user/cubit/user_cubit.dart';
-import 'package:everything_about_flutter_bloc/service/api_service.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'modules/user/bloc/user_bloc.dart';
 import 'modules/user/view/user_view.dart';
 
 void main() {
@@ -16,9 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => UserCubit(apiService: ApiService()))
-      ],
+      providers: [BlocProvider(create: (context) => UserBloc())],
       child: MaterialApp(
         title: 'FLUTTER BLOC',
         theme: ThemeData(
